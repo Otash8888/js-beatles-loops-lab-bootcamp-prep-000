aec2000eb9dff320fb4455e4011502be36e56dfa
Otabek Kamalov <otakam86@gmail.com>
2018-06-15 04:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts (arrayFacts) {
   var l=arrayFacts.length;
   
   while (l>0){
-   arrayFacts[l-1].push('!!!'); 
+   arrayFacts[l-1]=`${arrayFacts[l-1]} + '!!!'`; 
    --l;
   }
   return arrayFacts
