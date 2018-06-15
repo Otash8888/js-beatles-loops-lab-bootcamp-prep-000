afec6ede5c002d8a83c86e144d02f31d25ba8149
Otabek Kamalov <otakam86@gmail.com>
2018-06-15 04:05:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(arrMusicians,arrInstruments){
 var newArrayMusiciansAndInstruments = [];
+
 for (let i=0;i<arrMusicians.length;i++){
 newArrayMusiciansAndInstruments[i]=`${arrMusicians[i]} plays ${arrInstruments[i]}`
 }
